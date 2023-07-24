a54847e853cbe546c1194398dd3cd63d697195f8
guilleTosello <136275637+guilleTosello@users.noreply.github.com>
2023-07-24 11:07:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/biblioteca', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
 
 //Importando el Router libros
 const librosRouter = require('./routes/libros');
